97ae654e3aba296933d396dcfc00a8757f58cbf7
Sayyed Abdulrazzaque <kadiriabdulrazzaque@gmail.com>
2022-07-18 01:17:58 +0530

    Modified getMenu method

--- Restaurant_Finder_project/Restaurant_project -Student version/src/main/java/Restaurant.java
+++ Restaurant_Finder_project/Restaurant_project -Student version/src/main/java/Restaurant.java
@@ -18,15 +18,16 @@ public class Restaurant {
     }
 
     public boolean isRestaurantOpen() {
-        return true;
+        LocalTime currentTime = getCurrentTime();
+        return openingTime.isBefore(currentTime) && closingTime.isBefore();
+        //return true;
         //DELETE ABOVE STATEMENT AND WRITE CODE HERE
     }
 
     public LocalTime getCurrentTime(){ return  LocalTime.now(); }
 
     public List<Item> getMenu() {
-        return null;
-        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+        return menu;
     }
 
     private Item findItemByName(String itemName){
