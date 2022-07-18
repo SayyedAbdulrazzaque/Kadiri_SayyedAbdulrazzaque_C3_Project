c36b40fb0b37ab04f6a3482f490a2861e706c38c
Sayyed Abdulrazzaque <kadiriabdulrazzaque@gmail.com>
2022-07-18 23:50:14 +0530

    Part 2: Solution

--- Restaurant_Finder_project/Restaurant_project -Student version/src/test/java/RestaurantTest.java
+++ Restaurant_Finder_project/Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -1,9 +1,12 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.security.Provider;
 import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class RestaurantTest {
     Restaurant restaurant;
@@ -12,7 +15,7 @@ class RestaurantTest {
     public void beforeEach(){
         LocalTime openingTime = LocalTime.parse("10:30:00");
         LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant =Mockito.spy(new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime));
         restaurant.addToMenu("Sweet corn soup",119);
         restaurant.addToMenu("Vegetable lasagne", 269);
     }
@@ -21,13 +24,14 @@ class RestaurantTest {
     //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
+        Mockito.doReturn(LocalTime.parse("12:00:00")).when(restaurant).getCurrentTime();
+        assertTrue(restaurant.isRestaurantOpen());
     }
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
-
+        Mockito.doReturn(LocalTime.parse("23:00:00")).when(restaurant).getCurrentTime();
+        assertFalse(restaurant.isRestaurantOpen());
     }
 
     //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
